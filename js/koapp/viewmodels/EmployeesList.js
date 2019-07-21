79a9881aa983d136639eee7a735df1d701bc9292
Muqsith Irfan <mui@one.com>
2019-07-21 08:54:15 +0400

    Added more tests

--- js/koapp/viewmodels/EmployeesList.js
+++ js/koapp/viewmodels/EmployeesList.js
@@ -4,6 +4,12 @@ class EmployeeList {
 
     constructor(params) {
         this.firstName = params.firstName;
+        this.lastName = ko.observable('');
+        this.friends = ko.observableArray([]);
+        this.friends.push('Sreekanth');
+        console.log(this.friends());
+        this.friends.splice(0, this.friends().length);
+        console.log(this.friends());
     }
 
 }
